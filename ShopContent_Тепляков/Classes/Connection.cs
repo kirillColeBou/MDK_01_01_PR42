f1a93c3155fec837f16b5ab98ab34f2f4e4de729
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-04-27 22:23:27 +0500

    Разработанные классы общего пользования. Тепляков

---
+++ ShopContent_Тепляков/Classes/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ShopContent_Тепляков.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=USER\\SQLEXPRESS;Trusted_Connection=No;DataBase=ShopContent;User=sa;pwd=sa";
+
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
