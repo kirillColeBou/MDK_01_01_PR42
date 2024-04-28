a189989889597fd6f0de3141e3d3f2c83fe0030c
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-04-28 14:45:16 +0500

    Разработанные модели представления. Тепляков

---
+++ ShopContent_Тепляков/ViewModell/VMCategorys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ShopContent_Тепляков.ViewModell
+{
+    public class VMCategorys : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.CategorysContext> Categorys { get; set; }
+
+        public VMCategorys() => Categorys = Context.CategorysContext.AllCategorys();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
