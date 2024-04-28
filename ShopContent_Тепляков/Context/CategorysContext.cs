12b7d662ad4791f12f389a44b4fe714fbda33c58
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-04-28 14:38:18 +0500

    Разработанные контексты данных. Тепляков

---
+++ ShopContent_Тепляков/Context/CategorysContext.cs
@@ -0,0 +1,29 @@
+﻿using ShopContent_Тепляков.Modell;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ShopContent_Тепляков.Context
+{
+    public class CategorysContext : Categorys
+    {
+        public static ObservableCollection<CategorysContext> AllCategorys()
+        {
+            ObservableCollection<CategorysContext> allCategorys = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategorys = Classes.Connection.Query("SELECT * FROM Categorys", out connection);
+            while (dataCategorys.Read())
+            {
+                allCategorys.Add(new CategorysContext()
+                {
+                    Id = dataCategorys.GetInt32(0),
+                    Name = dataCategorys.GetString(1)
+                });
+            }
+            Classes.Connection.CloseConnection(connection);
+            return allCategorys;
+        }
+    }
+}
